8319562199238316019ed2fabebe1e6275f80069
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2022-10-18 02:38:24 +0800

    feat: installed relevant packages and created test index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  response.json(note)
+})
+
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
