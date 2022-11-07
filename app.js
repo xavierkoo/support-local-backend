f2a187346985f5eca4308df6164cc4fc2dbc9e55
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2022-11-07 23:11:26 +0800

    feat: added get routers for orders

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/api/merchants', merchantsRouter);
 app.use('/api/reviews', reviewsRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/login', loginRouter);
-app.use('/api/order', ordersRouter);
+app.use('/api/orders', ordersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- controllers/orders.js
+++ controllers/orders.js
@@ -2,4 +2,25 @@ const ordersRouter = require('express').Router();
 const Order = require('../models/order');
 const logger = require('../utils/logger');
 
+// retrieve all order
+ordersRouter.get('/', async (request, response) => {
+    const orders = await Order
+        .find({}).populate('user').populate('products');
+    if (orders) {
+        response.json(orders);
+    } else {
+        response.status(404).end();
+    }
+});
+
+// retrieve specific order
+ordersRouter.get('/:id', async (request, response) => {
+    const order = await Order.findById(request.params.id);
+    if (order) {
+        response.json(order);
+    } else {
+        response.status(404).end();
+    }
+});
+
 module.exports = ordersRouter;
