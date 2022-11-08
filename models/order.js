38707bec1380b6e3ddcac0ec726117e6af2c119b
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2022-11-08 17:22:28 +0800

    fix: modified order schema userid field

--- models/order.js
+++ models/order.js
@@ -5,8 +5,7 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const orderSchema = mongoose.Schema({
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        type: String,
     },
     orderDate: {
         type: String,
