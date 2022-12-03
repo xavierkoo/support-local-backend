2e29a8fe0158fef0b1466cb27e74eac60f444822
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2022-12-04 02:42:11 +0800

    feat: added test folder and files and set up config for integration testing

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,11 @@
 require('dotenv').config();
 
 // environment variables for port and mongodb
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT;
+
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = {
     MONGODB_URI,
