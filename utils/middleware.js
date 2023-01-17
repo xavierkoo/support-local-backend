283825af5ccf2456ff65c838ffe83a3eda6fb6e5
Xavier Koo <86020207+xavierkoo@users.noreply.github.com>
2023-01-17 23:33:20 +0800

    Update middleware.js

--- utils/middleware.js
+++ utils/middleware.js
@@ -22,23 +22,12 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: 'malformatted id' });
     } if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message });
-    } if (error.name === 'JsonWebTokenError') {
-        return response.status(401).json({
-            error: 'invalid token',
-        });
-    } if (error.name === 'TokenExpiredError') {
-        return response.status(401).json({
-            error: 'token expired',
-        });
     }
-
     next(error);
 };
 
 module.exports = {
     requestLogger,
     unknownEndpoint,
-    // tokenExtractor,
-    // userExtractor,
     errorHandler,
 };
